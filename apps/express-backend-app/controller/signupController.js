e4963460eb196df9920ad408004e0f24078c40c3
R Amarthya Sreechand <amarthyasreechand@gmail.com>
2025-08-10 02:13:53 +0530

    Hash and Store the Signup password

--- apps/express-backend-app/controller/signupController.js
+++ apps/express-backend-app/controller/signupController.js
@@ -1,5 +1,7 @@
 const userStorage = require('../model/db.js');
 const { body, validationResult } = require('express-validator');
+const { signupHash } = require('../service/signupHash.js');
+const signupRouter = require('../routers/signupRouter.js');
 
 exports.userSignupGet = (req, res) => {
   res.render('index', {
@@ -30,7 +32,7 @@ const validateUser = [
 // to the client
 exports.addUserPost = [
   ...validateUser,
-  function(req, res, next) {
+  async function(req, res, next) {
     const result = validationResult(req);
     if (!result.isEmpty()) {
       // Here, I didn't redirect, because, there is no perceived problem even if the person refreshes and resubmits the post request.
@@ -39,9 +41,23 @@ exports.addUserPost = [
         message: 'Welcome to the world of Algorithms',
       });
     }
-    userStorage.addUser(req.body);
+
+
+    const userDetail = req.body;
+    let hashedPass;
+    try {
+      hashedPass = await signupHash(userDetail.password);
+    } catch (err) {
+      return res.status(500).send(`${err}`);
+    };
+
+    //Changing the userDetail object's password property's value to hashed value
+    userDetail.password = hashedPass;
+
+    userStorage.addUser(userDetail);
     res.redirect(`${req.baseUrl}/database`);
-  }]
+  },
+]
 
 // A Middleware to display updated database in the redirect GET.
 exports.displayUserGet = function(req, res) {
